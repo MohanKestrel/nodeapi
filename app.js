9195a3be2bceddda3e9a607bf8a9c89ed615d100
Mohanraj S <mohanok6@gmail.com>
2022-07-18 23:36:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const mysql = require('mysql')
 
 const app = express()
-const port = process.env.PORT || 443
+const port = process.env.PORT || 8888
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
